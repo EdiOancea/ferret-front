331eaf38f64ed971c101d8b9788018ef448e16f5
Edi Oancea <eduard.oancea@algotech.solutions>
2019-05-06 12:00:35 +0300

    Renamed prop and updated tests.

--- app/components/SignInPage/index.js
+++ app/components/SignInPage/index.js
@@ -1,35 +1,13 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import withStyles from '@material-ui/core/styles/withStyles';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 
 import PaperFrame from 'components/PaperFrame';
 import SignInForm from 'containers/SignInForm';
 
-const styles = theme => ({
-  main: {
-    width: 'auto',
-    display: 'block', // Fix IE 11 issue.
-    marginLeft: theme.spacing.unit * 3,
-    marginRight: theme.spacing.unit * 3,
-    [theme.breakpoints.up(400 + theme.spacing.unit * 3 * 2)]: {
-      width: 400,
-      marginLeft: 'auto',
-      marginRight: 'auto',
-    },
-  },
-});
-
-const SignInPage = ({ classes }) => (
-  <div className={classes.main}>
-    <PaperFrame text="Sign in" icon={<LockOutlinedIcon />}>
-      <SignInForm />
-    </PaperFrame>
-  </div>
+const SignInPage = () => (
+  <PaperFrame headerText="Sign in" icon={<LockOutlinedIcon />}>
+    <SignInForm />
+  </PaperFrame>
 );
 
-SignInPage.propTypes = {
-  classes: PropTypes.object.isRequired,
-};
-
-export default withStyles(styles)(SignInPage);
+export default SignInPage;
