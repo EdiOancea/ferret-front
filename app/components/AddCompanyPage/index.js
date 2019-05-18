788f08b36b52ca444724c792d57a56965f909bb4
Darius Lauran <darius.lauran@algotech.solutions>
2019-05-18 08:50:13 +0300

    Add Company component with tests.

---
+++ app/components/AddCompanyPage/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import CreateOutlinedIcon from '@material-ui/icons/CreateOutlined';
+
+import PaperFrame from 'components/PaperFrame';
+
+const AddCompanyPage = () => (
+  <PaperFrame headerText="Add Company" icon={<CreateOutlinedIcon />} />
+);
+
+export default AddCompanyPage;
