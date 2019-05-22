4a0eaa197d6f178a09c59e96c3cf6a9de8979ccd
Darius Lauran <darius.lauran@algotech.solutions>
2019-05-22 14:54:11 +0300

    Remove files not from origin/master.

--- app/containers/AddCompanyForm/selectors.js
+++
@@ -1,7 +0,0 @@
-import { createSelector } from 'reselect';
-
-export const selectAddCompany = state => state.get('addCompany');
-
-export const selectErrorMessage = createSelector(selectAddCompany, addCompany =>
-  addCompany.get('errorMessage'),
-);
--- app/services/__mocks__/addCompanyRequest.js
+++
@@ -1,7 +0,0 @@
-import { company } from '../../../tools/mockData';
-
-const addCompanyRequest = jest.fn(
-  data => (data === 1 ? company : { message: 'message' }),
-);
-
-export default addCompanyRequest;
