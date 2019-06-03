8ca4421f9a6df7e5aa7441530522ecbb5ed9ad08
Darius Lauran <darius.lauran@algotech.solutions>
2019-06-03 12:05:08 +0300

    Remove files not from origin/master.

--- app/services/__mocks__/getCompanyRequest.js
+++
@@ -1,7 +0,0 @@
-import { company } from '../../../tools/mockData';
-
-const getCompanyRequest = jest.fn(
-  id => (id === '1' ? company : { message: 'message' }),
-);
-
-export default getCompanyRequest;
