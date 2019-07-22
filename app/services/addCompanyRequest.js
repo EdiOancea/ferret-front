ce8c5e44d94722bf6ee47c2b681a54981a7fa664
Darius Lauran <darius.lauran@algotech.solutions>
2019-07-22 13:43:14 +0300

    Append non-file properties of the new company to formData.

--- app/services/addCompanyRequest.js
+++ app/services/addCompanyRequest.js
@@ -5,7 +5,7 @@ const addCompanyRequest = async data => {
   const files = [...fileList];
   const formData = new FormData();
   files.forEach(file => formData.append('images', file, file.name));
-  Object.keys(data).forEach(key => formData.append(key, data[key]));
+  Object.keys(bodyData).forEach(key => formData.append(key, bodyData[key]));
 
   return apiRequest.post('/companies/', formData, undefined, {
     'Content-Type': 'multipart/form-data',
