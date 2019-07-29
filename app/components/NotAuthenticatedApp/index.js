289aa0b9c3d4287a69b98828e6ded868af233cdf
Darius Lauran <darius.lauran@algotech.solutions>
2019-07-29 21:35:11 +0300

    Format the company addresses for view.

--- app/components/NotAuthenticatedApp/index.js
+++ app/components/NotAuthenticatedApp/index.js
@@ -2,11 +2,15 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
 import SignInPage from 'components/SignInPage/Loadable';
+import CompanyPage from 'components/CompanyPage/Loadable';
+import AddCompanyPage from 'components/AddCompanyPage/Loadable';
 import NotFoundPage from 'containers/NotFoundPage/Loadable';
 import HomePage from 'containers/HomePage/Loadable';
 
 const NotAuthenticatedApp = () => (
   <Switch>
+    <Route path="/companies/:id" component={CompanyPage} />
+    <Route path="/add-company" component={AddCompanyPage} />
     <Route path="/signin" component={SignInPage} />
     <Route path="/home" component={HomePage} />
     <Route component={NotFoundPage} />
